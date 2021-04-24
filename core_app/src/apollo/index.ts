a70ee6c6f644cddcad5843d0558a579ad63009ff
jungai <juworaphol125@gmail.com>
2021-04-24 18:27:35 +0700

    remove express thing & update apollo

--- core_app/src/apollo/index.ts
+++ core_app/src/apollo/index.ts
@@ -1,42 +1,74 @@
-import { ApolloServer, gql } from 'apollo-server-express';
-import { DocumentNode } from 'graphql';
-import { Express } from 'express';
-import { app } from '../express';
+import { ApolloServer, gql, makeExecutableSchema } from 'apollo-server';
+import { DocumentNode, GraphQLResolveInfo, GraphQLSchema } from 'graphql';
+
+// See below about resolver functions.
+type GraphQLFieldResolveFn = (
+	source?: any,
+	args?: { [argName: string]: any },
+	context?: any,
+	info?: GraphQLResolveInfo
+) => any;
+
+const books: { title: string; author: string }[] = [
+	{
+		title: 'The Awakening',
+		author: 'Kate Chopin'
+	},
+	{
+		title: 'City of Glass',
+		author: 'Paul Auster'
+	},
+	{
+		title: 'test',
+		author: 'ju'
+	}
+];
 
 export function setupTypeDef(): DocumentNode {
 	// Construct a schema, using GraphQL schema language
 	const typeDefs = gql`
+		type Book {
+			title: String
+			author: String
+		}
+
+		type Author {
+			name: String
+			books: [Book]
+		}
+
 		type Query {
-			hello: String
+			getBooks(title: String): [Book]
 		}
 	`;
 
 	return typeDefs;
 }
-
 export function setupResolvers() {
-	// Provide resolver functions for your schema fields
-
-	const resolvers = {
+	const resolvers: {
 		Query: {
-			hello: () => 'Hello world!'
+			getBooks: GraphQLFieldResolveFn;
+		};
+	} = {
+		Query: {
+			getBooks: () => {
+				return books;
+			}
 		}
 	};
 
 	return resolvers;
 }
 
-export async function getApolloServer(): Promise<{
-	app: Express;
-	server: ApolloServer;
-}> {
-	const server = new ApolloServer({
+export function getSchema(): GraphQLSchema {
+	return makeExecutableSchema({
 		typeDefs: setupTypeDef(),
 		resolvers: setupResolvers()
 	});
-	await server.start();
+}
 
-	server.applyMiddleware({ app, path: '/jugql' });
+export async function getApolloServer(): Promise<ApolloServer> {
+	const server = new ApolloServer({ schema: getSchema() });
 
-	return { app, server };
+	return server;
 }
